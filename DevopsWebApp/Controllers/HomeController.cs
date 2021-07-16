af2b99221a7cd102f24b4f1164b9aa2a1c77e1c7
Aayush-gupta10 <aayush.gupta01@nagarro.com>
2021-07-16 22:20:46 +0530

    Add project files.

---
+++ DevopsWebApp/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DevopsWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DevopsWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+
+        public  IActionResult Book()
+        {
+
+            List<BookModel> books = new List<BookModel>();
+            books.Add(new BookModel { Author = "Mark Twain", BookId = 1, BookName = "Adventures of Tom Sawyer" });
+            books.Add(new BookModel { Author = "George Orwell", BookId = 2, BookName = "Animal Farm" });
+            return View(books);
+        }
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ XUnitTestProject1/UnitTest1.cs
@@ -0,0 +1,25 @@
+using DevopsWebApp.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using Xunit;
+
+namespace XUnitTestProject1
+{
+    public class UnitTest1
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        [Fact]
+        public void Test1()
+        {
+            var controller = new HomeController(_logger);
+
+            //Act  
+            var data = controller.Book();
+
+            //Assert  
+            Assert.IsType<OkObjectResult>(data);
+        }
+    }
+}
